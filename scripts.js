6c145f9b2c89f069156714c8eacf8f8a400404b0
Javier <javito774@gmail.com>
2024-05-20 21:05:37 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+if (true) {
+    let edad = 30;
+    edad = 24;
+    const ciudad = "Madrid";
+}
+
+let edad = 56;
+
+const canciones = [
+    {
+        nombre: "Pepito",
+        autor: "Fulanito"
+    }
+]
+
+let cancion1Nombre = canciones[0].nombre;
